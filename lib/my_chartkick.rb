aa457e9ade8739d100657e53bc5d05a14a49a4b1
ken <block24block@gmail.com>
2017-04-01 17:10:24 +0800

    colors

--- lib/my_chartkick.rb
+++ lib/my_chartkick.rb
@@ -1,12 +1,15 @@
 require "my_chartkick/version"
 require 'my_chartkick/data_set'
+require 'my_chartkick/rainbow'
 require 'chartkick'
 
 module MyChartkick
 
   Chartkick::Helper.instance_methods.each do |method|
     define_method "my_#{method}" do |data, opt|
+      opt = opt.dup
       data_set = DataSet.create data, opt
+      opt.merge!({colors: Rainbow[data_set.count]}) if Array === data_set
       send method, data_set, opt
     end
   end
