573b406cebcf375d849a7f72ad7c641162e52409
ken <block24block@gmail.com>
2017-04-01 16:11:38 +0800

    use chartkick

--- lib/my_chartkick.rb
+++ lib/my_chartkick.rb
@@ -1,6 +1,15 @@
 require "my_chartkick/version"
 require 'my_chartkick/data_set'
+require 'chartkick'
 
 module MyChartkick
-  # Your code goes here...
+
+  Chartkick::Helper.instance_methods.each do |method|
+    define_method "my_#{method}" do |data, opt|
+      data_set = DataSet.create data, opt
+      send method, data_set, opt
+    end
+  end
+
+  include Chartkick::Helper
 end
--- test/my_chartkick_test.rb
+++ test/my_chartkick_test.rb
@@ -4,4 +4,23 @@ class MyChartkickTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::MyChartkick::VERSION
   end
+
+  attr_reader :data
+
+  def setup
+    @data = (1..10).to_a
+  end
+
+  include MyChartkick
+
+  def test_common_chart
+    %w{line column bar area}.each_with_index do |ch, i|
+      i = i + 1
+      puts send "my_#{ch}_chart", data, x: :odd_or_even, id: i.to_s
+      puts send "my_#{ch}_chart", data, x: :odd_or_even, y: :gt9, id: (i * 10).to_s
+    end
+
+    puts send "my_pie_chart", data, x: :odd_or_even, id: '99'
+  end
+
 end
