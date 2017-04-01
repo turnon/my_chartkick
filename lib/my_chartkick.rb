770be9f841cac75e75b09c8ecfea6f5ac4dd4f05
ken <block24block@gmail.com>
2017-04-01 23:21:05 +0800

    use rand_palette

--- lib/my_chartkick.rb
+++ lib/my_chartkick.rb
@@ -1,6 +1,6 @@
 require "my_chartkick/version"
 require 'my_chartkick/data_set'
-require 'my_chartkick/rainbow'
+require 'rand_palette'
 require 'chartkick'
 
 module MyChartkick
@@ -9,7 +9,7 @@ module MyChartkick
     define_method "my_#{method}" do |data, opt|
       opt = opt.dup
       data_set = DataSet.create data, opt
-      opt.merge!({colors: Rainbow[data_set.count]}) if Array === data_set
+      opt.merge!({colors: RandPalette.random(data_set.count, alpha: 0.8)}) if Array === data_set
       send method, data_set, opt
     end
   end
